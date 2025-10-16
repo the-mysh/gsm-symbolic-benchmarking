ca0cefda8b9ed3680a88ea4f65b2f08786da7676
mysh <mysh995@gmail.com>
2025-10-16 15:55:15 +0100

    Refactored benchmark runner

--- src/gsm_benchmarker/benchmark.py
+++ src/gsm_benchmarker/benchmark.py
@@ -4,6 +4,7 @@ import os
 import pandas as pd
 import torch
 from pathlib import Path
+from functools import cached_property
 
 from gsm_benchmarker.dataset_wrapper import GSMSymbolicDataset
 from gsm_benchmarker.benchmark_config import BenchmarkConfig
@@ -25,15 +26,27 @@ class BenchmarkRunner:
 
         self._results = {}
 
-    def run(self, n_sets: int | None = None, n_per_set: int | None = None):
-        # TODO: refactor
+    @cached_property
+    def intermediate_storage_path(self):
+        p = self._storage_path / 'intermediate'
+        confirm_or_create_folder(p)
+        return p
 
+    @cached_property
+    def final_storage_path(self):
+        p = self._storage_path / 'final'
+        confirm_or_create_folder(p)
+        return p
+
+    def _pre_populate_results(self):
+        # results structure: variants as outer layer, models as inner
+        # but for efficiency, evaluate per-model (loading models takes longer)
         for variant in self._dset_variants:
             if variant not in self._results:
                 self._results[variant] = {}
 
-        intermediate_storage_path = self._storage_path/'intermediate'
-        final_storage_path = self._storage_path/'results'
+    def run(self, n_sets: int | None = None, n_per_set: int | None = None):
+        self._pre_populate_results()
 
         for im, model in enumerate(self._models):
             logger.info(f"{10*'='} Evaluating model {im+1}/{len(self._models)}: {model} {10*'='}")
@@ -46,29 +59,40 @@ class BenchmarkRunner:
                 dataset_wrapper = GSMSymbolicDataset(variant=variant)
                 eval_sets = dataset_wrapper.create_evaluation_sets(n_sets=n_sets, n_per_set=n_per_set)
 
+                # Evaluate model
                 res = model_evaluator.evaluate_multiple_datasets(
                     eval_sets,
-                    intermediate_storage_path=intermediate_storage_path / f"{model_evaluator.path_friendly_model_name}"
+                    intermediate_storage_path=self.intermediate_storage_path / f"{model_evaluator.path_friendly_model_name}"
                 )
 
-                logger.debug("Storing results")
+                # Store results
                 self._results[variant][model] = res
-
-                res_path = final_storage_path / f"results/{dataset_wrapper.DSET_NAME}_{dataset_wrapper.variant_name}"
-                os.makedirs(res_path, exist_ok=True)
-                fname = res_path / f"{model_evaluator.path_friendly_model_name}.parquet"
-                res.to_parquet(fname)
-                logger.debug(f"Model x variant results stored to: {fname}")
+                self._store_model_x_variant_result(dataset_wrapper, model_evaluator, res)
 
                 logger.info(f"Evaluation of model {model} on variant {variant} completed")
+                self._delete_model(model_evaluator)
 
             logger.info(f"Evaluation of model {model} on all dataset variants completed")
-            logger.debug("Deleting model from memory")
-            del model_evaluator.model_wrapper
-            torch.cuda.empty_cache()
 
         logger.info("EVALUATION COMPLETE")
         return self._results
+
+    @staticmethod
+    def _delete_model(model_evaluator):
+        logger.debug("Deleting model from memory")
+        del model_evaluator.model_wrapper
+        torch.cuda.empty_cache()
+
+    def _store_model_x_variant_result(self, dataset_wrapper, model_evaluator, res):
+        logger.debug("Storing results")
+
+        res_path = self.final_storage_path / f"results/{dataset_wrapper.DSET_NAME}_{dataset_wrapper.variant_name}"
+        confirm_or_create_folder(res_path)
+
+        fname = res_path / f"{model_evaluator.path_friendly_model_name}.parquet"
+        res.to_parquet(fname)
+
+        logger.debug(f"Model x variant results stored to: {fname}")
 
 
 def run_full_benchmark():
