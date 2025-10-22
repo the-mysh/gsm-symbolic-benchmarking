f4f683b2a70769ac5cb104bef3645e1e365982b5
mysh <mysh995@gmail.com>
2025-10-22 14:38:10 +0100

    Load models results in init

--- src/gsm_benchmarker/results_analyser.py
+++ src/gsm_benchmarker/results_analyser.py
@@ -2,16 +2,24 @@ import os
 import logging
 import pandas as pd
 from pathlib import Path
+from tqdm import tqdm
 
 
 logger = logging.getLogger(__name__)
 
 
 class ModelResultsAnalyser:
-    def __init__(self, data: pd.DataFrame):
+    def __init__(self, file_path: str | Path):
+        self._file_path = Path(file_path)
+
+        data = self.load_data(self._file_path)
         self._check_data(data)
         self._data = data
         self._data.index.names = ['set', 'question']  # TODO: move to where it's saved
+
+    @staticmethod
+    def load_data(file_path: str | Path) -> pd.DataFrame:
+        return pd.read_parquet(file_path)
 
     @staticmethod
     def _check_data(data: pd.DataFrame):
@@ -19,7 +27,7 @@ class ModelResultsAnalyser:
             raise TypeError(f"Expected a pandas.DataFrame, got {type(data)}: {data}")
 
         if 'correct' not in data.columns:
-            raise ValueError("The provided dataframe is missing a 'correct' column")
+            raise ValueError("The results dataframe is missing a 'correct' column")
 
         if not isinstance(data.index, pd.MultiIndex) or data.index.nlevels != 2:
             raise ValueError("The results dataframe must have a 2-level multiindex")
@@ -27,10 +35,6 @@ class ModelResultsAnalyser:
     @property
     def data(self) -> pd.DataFrame:
         return self._data
-
-    @classmethod
-    def from_file(cls, file_path: str | Path):
-        return cls(pd.read_parquet(file_path))
 
     @property
     def correctness(self) -> pd.Series:
@@ -52,27 +56,29 @@ class ModelResultsAnalyser:
 
 
 class MultiModelResultsAnalyser:
-    def __init__(self, data: pd.DataFrame):
-        self._data = data
+    def __init__(self, dir_path: str | Path):
+        self._dir_path = Path(dir_path)
+        self._summary_data = self.load_summary_data(self._dir_path)
 
     @property
-    def data(self) -> pd.DataFrame:
-        return self._data
+    def summary_data(self) -> pd.DataFrame:
+        return self._summary_data
 
-    @classmethod
-    def from_multi_model_directory(cls, dir_path: str | Path):
+    @staticmethod
+    def load_summary_data(dir_path: str | Path):
         dir_path = Path(dir_path).resolve()
 
         data_dict = {}
 
-        for item_name in os.listdir(dir_path):
+        logger.debug("Loading per-model results")
+        for item_name in tqdm(os.listdir(dir_path), desc="Model"):
             item_path = dir_path / item_name
             if item_path.is_dir():
                 logger.warning(f"The algorithm is not meant for non-flat directories; found subfolder '{item_name}'")
                 continue
-            model_results = ModelResultsAnalyser.from_file(item_path)
+            model_results = ModelResultsAnalyser(item_path)
             s = model_results.get_total_accuracy_and_std()
             data_dict[''.join(item_name.split('.')[:-1])] = {'accuracy': s[0], 'std': s[1]}
 
         data_df = pd.DataFrame(data_dict)
-        return cls(data_df.T)
+        return data_df.T
