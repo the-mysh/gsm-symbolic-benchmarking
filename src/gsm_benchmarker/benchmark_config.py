45c4865ded5b37a2cb8d78d17c72eac80878c8f6
mysh <dominika.dlugosz@tecnico.ulisboa.pt>
2025-11-06 16:03:23 +0000

    Benchmark config - get memory settings automatically for a specific machine

--- src/gsm_benchmarker/benchmark_config.py
+++ src/gsm_benchmarker/benchmark_config.py
@@ -1,5 +1,16 @@
 from dataclasses import dataclass
 import torch
+from typing import Any
+import logging
+
+from gsm_benchmarker.utils.resources_manager import load_resource_json
+
+
+_UNDEFINED = object()
+_AUTO = object()
+
+logger = logging.getLogger(__name__)
+
 
 
 @dataclass
@@ -10,25 +21,98 @@ class BenchmarkConfig:
     temperature: float = 0.0  # greedy decoding
     max_new_tokens: int = 1024
     batch_size: int = 1
-    device: str = "cuda" if torch.cuda.is_available() else "cpu"
+    device: str = ""
 
     # Model-specific
     use_4bit: bool = True  # For memory efficiency
     trust_remote_code_global: bool = False
 
     # memory settings
-    gpu0_max_memory: str | None = "7GB"
-    gpu1_max_memory: str | None = None
-    cpu_max_memory: str = "12GB"
+    gpu_max_memory: str | None = "7GiB"
+    gpu_index: int | None = _UNDEFINED
+    cpu_max_memory: str = "12GiB"
+
+    def __post_init__(self):
+        cuda_available = torch.cuda.is_available()
+
+        if not self.device:
+            self.device = "cuda" if cuda_available else "cpu"
+
+        if self.gpu_index is _UNDEFINED:
+            self.gpu_index = 0 if cuda_available else None
 
     @property
     def memory_settings(self):
-        mem = {"cpu": self.cpu_max_memory}
-        if self.gpu0_max_memory is not None:
-            mem[0] = self.gpu0_max_memory
-        
-        if self.gpu1_max_memory is not None:
-                mem[1] = self.gpu1_max_memory
+        mem: dict[str | int, str] = {"cpu": self.cpu_max_memory}
+
+        if self.gpu_index is not None:
+            if not self.gpu_max_memory:
+                raise RuntimeError("gpu_max_memory is not defined")
+            mem[self.gpu_index] = self.gpu_max_memory
         
         return mem
-    
+
+    @classmethod
+    def for_machine(cls, machine_name: str, gpu_index: int | None | type[_AUTO] = _AUTO, ram_margin=4, vram_margin=2,
+                    **kwargs: Any) -> "BenchmarkConfig":
+
+        machines_config = load_resource_json('machines_config.json')
+
+        machine_type: str | None = machines_config['machines'].get(machine_name, None)
+        if not machine_type:
+            raise ValueError(f"No configuration defined for machine name '{machine_name}'")
+
+        logger.debug(f"Reading config for machine '{machine_name}', of type '{machine_type}'")
+
+        machine_params: dict | None = machines_config['machine_types'].get(machine_type, None)
+        if not machine_params:
+            raise RuntimeError(f"No configuration defined for machine type '{machine_type}'")
+
+        n_gpus = machine_params.get('gpus')
+        assert isinstance(n_gpus, int)
+
+        cpu_memory, gpu_memory = cls.get_max_memories(machine_params, gpu_index, ram_margin, vram_margin)
+
+        return BenchmarkConfig(
+            gpu_max_memory=f"{gpu_memory}GiB",
+            gpu_index=gpu_index,
+            cpu_max_memory=f"{cpu_memory}GiB",
+            **kwargs
+        )
+
+    @staticmethod
+    def validate_gpu_index(machine_name: str, n_gpus: int, gpu_index: int | None | type[_AUTO] = _AUTO) -> int | None:
+
+        if gpu_index is _AUTO:
+            if n_gpus:
+                logger.info("Setting default gpu index: 0")
+                return 0
+            logger.info("No GPUs available")
+            return None
+
+        if gpu_index is None:
+            if n_gpus:
+                logger.warning(f"gpu_index is set to None; none of the available {n_gpus} GPUs will be used")
+            return None
+
+        # gpu_index is not None
+        if gpu_index >= n_gpus:
+            raise ValueError(f"Cannot use GPU {gpu_index} for machine '{machine_name}' with a total of {n_gpus} GPUs")
+
+        return gpu_index
+
+    @staticmethod
+    def get_max_memories(machine_params: dict, gpu_index: int | None, ram_margin: int, vram_margin: int
+                         ) -> tuple[int, int | None]:
+        cpu_memory = machine_params.get('ram')
+        assert isinstance(cpu_memory, int)
+        cpu_memory -= ram_margin
+
+        if gpu_index is None:
+            gpu_memory = None
+        else:
+            gpu_memory = machine_params.get('vram')
+            assert isinstance(gpu_memory, int)
+            gpu_memory -= vram_margin
+
+        return cpu_memory, gpu_memory
---
+++ src/gsm_benchmarker/utils/resources_manager.py
@@ -0,0 +1,17 @@
+import logging
+from importlib.resources import files
+import json
+from typing import Any
+from pathlib import Path
+
+
+logger = logging.getLogger(__name__)
+_RESOURCES_PATH = files("gsm_benchmarker")/"resources"
+
+
+def load_resource_json(file_name: str) -> dict[str, Any]:
+    full_path = _RESOURCES_PATH / file_name
+    logger.debug(f"Loading resource from file: {Path(full_path).resolve()}")
+    data_bytes = full_path.read_bytes()
+    data_dict = json.loads(data_bytes)
+    return data_dict
