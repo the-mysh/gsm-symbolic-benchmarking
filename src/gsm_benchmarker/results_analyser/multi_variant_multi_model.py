f53709c6f81f0bb41cb2b131baf8b8cf4e065b33
mysh <dominika.dlugosz@tecnico.ulisboa.pt>
2025-11-12 23:03:26 +0000

    Results analysis: load summary data from entire benchmark

--- src/gsm_benchmarker/results_analyser/multi_variant_multi_model.py
+++ src/gsm_benchmarker/results_analyser/multi_variant_multi_model.py
@@ -16,11 +16,15 @@ class MultiVariantMultiModelResultsAnalyser:
 
     def __init__(self, dir_path: str | Path):
         self._dir_path = Path(dir_path).resolve()
-        self._summary_data = self._load_summary_data(self._dir_path)
+        self._summary_data, self._comparison_data = self._load_data(self._dir_path)
 
     @property
     def summary_data(self):
         return self._summary_data
+
+    @property
+    def comparison_data(self):
+        return self._comparison_data
 
     @classmethod
     def match_variant_name(cls, name):
@@ -30,16 +34,27 @@ class MultiVariantMultiModelResultsAnalyser:
         return match.group('variant')
 
     @classmethod
-    def _load_summary_data(cls, dir_path: Path) -> pd.DataFrame:
-        data_dict = {}
+    def _load_data(cls, dir_path: Path) -> tuple[pd.DataFrame, pd.DataFrame]:
+        summary_data_dict = {}
+        comparative_data_dict = {}
 
         logger.debug("Loading per-model results")
         for item_name in tqdm(os.listdir(dir_path), desc="Model"):
             item_path = dir_path / item_name
             if not item_path.is_dir():
                 continue
-            multi_model_results = MultiModelResultsAnalyser(item_path)
-            data_dict[cls.match_variant_name(item_name)] = multi_model_results.summary_data
+            multi_model_results = MultiModelResultsAnalyser(item_path, load_full_data=True)
+            v = cls.match_variant_name(item_name)
+            summary_data_dict[v] = multi_model_results.summary_data
 
-        df = pd.concat(data_dict.values(), keys=data_dict.keys(), axis=1)
-        return df
+            idx_frame = multi_model_results.full_data[['model', 'id', 'instance']]
+            s = multi_model_results.full_data['correct']
+            s.index = pd.MultiIndex.from_frame(idx_frame)
+            comparative_data_dict[v] = s
+
+        def concat(d: dict[str, pd.DataFrame | pd.Series]) -> pd.DataFrame:
+            return pd.concat(d.values(), keys=d.keys(), axis=1)
+
+        df_summary = concat(summary_data_dict)
+        df_comparison = concat(comparative_data_dict).reset_index()
+        return df_summary, df_comparison
