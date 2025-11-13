95fee4df592c5da2399eaa570c5a7a658917c3d8
mysh <dominika.dlugosz@tecnico.ulisboa.pt>
2025-11-13 15:13:37 +0000

    Added conftest.py

--- src/tests/test_gsm_benchmarker/test_shot_manager.py
+++ src/tests/test_gsm_benchmarker/test_shot_manager.py
@@ -3,45 +3,6 @@ import pytest
 from gsm_benchmarker.shot_manager import SingleShot, GSMShotManager
 
 
-MOCK_DATA = {
-    "comment": "Mock 8 shots for testing.",
-    "samples": [
-        {
-            "question": "Q1?",
-            "solution": "A1.",
-            "result": "11"
-        },
-        {
-            "question": "Q2?",
-            "solution": "A2.",
-            "result": "25"
-        },
-        {
-            "question": "Q3?",
-            "solution": "A3.",
-            "result": "39"
-        },
-    ]
-}
-
-
-@pytest.fixture
-def shot_manager():
-    return GSMShotManager()
-
-
-@pytest.fixture
-def mock_shot_manager(mocker):
-    mocker.patch(
-        "gsm_benchmarker.shot_manager.load_resource_json",
-        return_value=MOCK_DATA
-    )
-
-    # 2. Instantiate and return the object that uses the patched function
-    manager = GSMShotManager()
-    return manager
-
-
 def test_shots_init(shot_manager: GSMShotManager):
     assert isinstance(shot_manager._shots, tuple)
     assert len(shot_manager._shots) == 8
