cb2a4ec9aa66808ea6649e8a483305ea5566c138
mysh <dominika.dlugosz@tecnico.ulisboa.pt>
2026-04-15 19:44:45 +0100

    Formalised solution prompt

---
+++ src/gsm_benchmarker/resources/solutions_8shot/formalised.py
@@ -0,0 +1,134 @@
+
+SOLUTIONS = []
+
+solution1 = """
+Given:
+Number of trees currently in the grove: 15
+Number of trees in the grove after the workers are done planting today: 21
+
+To find: number of trees the grove workers will plant today.
+
+Solution:
+Calculate the difference between trees after planting and trees currently in the grove:
+21 - 15 = 6
+"""
+
+SOLUTIONS.append(solution1)
+
+
+solution2 = """
+Given:
+Number of cars originally in the parking lot: 3
+Number of cars arriving in the parking lot: 2
+    
+To find: total number of cars in the parking lot.
+
+Solution:
+Calculate the sum of cars originally in the parking lot and the arriving cars:
+3 + 2 = 5
+"""
+
+SOLUTIONS.append(solution2)
+
+solution3 = """
+Given:
+Number of chocolates Leah originally had: 32
+Number of chocolates Leah's sister originally had: 42
+Number of chocolates Leah and her sister ate: 35
+
+To find: total number of chocolates left.
+
+Solution:
+First, calculate how many chocolates the sisters originally had in total:
+32 + 42 = 74
+Next, subtract the total number of chocolates they ate:
+74 - 35 = 39
+"""
+
+SOLUTIONS.append(solution3)
+
+
+solution4 = """
+Given:
+Number of lollipops Jason had originally: 20
+Number of lollipops Jason has left: 12
+
+To find: number of lollipops Jason gave to Denny.
+
+Solution:
+Calculate the difference between the lollipops Jason originally had and the lollipops he has now:
+20 - 12 = 8
+"""
+
+SOLUTIONS.append(solution4)
+
+
+solution5 = """
+Given:
+Number of toys Shawn originally has: 5
+Number of toys his mom and his dad each gave him for Christmas: 2
+
+To find: total number of toys Shawn has now.
+
+Solution:
+First, calculate how many toys in total Shawn got for Christmas:
+2 * 2 = 4
+Next, calculate the sum of the number of toys Shawn originally had and the number of new toys he got:
+5 + 4 = 9
+"""
+
+
+SOLUTIONS.append(solution5)
+
+
+solution6 = """
+Given:
+Number of computers originally in the server room: 9
+Number of new computers installed each day: 5
+Number of days from monday to thursday: 4
+
+To find: total number of computers in the server room now.
+
+Solution:
+First, calculate how many new computers were installed in total:
+4 * 5 = 20
+Next, calculate the sum of the number of computers originally in the room and the total number of new computers installed:
+9 + 20 = 29
+"""
+
+SOLUTIONS.append(solution6)
+
+
+solution7 = """
+Given:
+Number of golf balls Michael originally had: 58
+Number of golf balls lost on tuesday: 23
+Number of golf balls lost on wednesday: 2
+
+To find: number of golf balls left at the end of wednesday.
+
+Solution:
+First, calculate the total number of golf balls Michael lost:
+23 + 2 = 25
+Next, calculate the difference between the original number of golf balls and the number of golf balls lost:
+58 - 25 = 33
+"""
+
+SOLUTIONS.append(solution7)
+
+solution8 = """
+Given:
+Number of dollars Olivia had originally: 23
+Number of bagels Olivia bought: 5
+Number of dollars each bagel cost: 3
+
+To find: how many dollars Olivia has left.
+
+Solution:
+First, calculate the total cost of the bagels:
+5 * 3 = 15
+Next, calculate the difference between Olivia's original money and what she paid for the bagels:
+23 - 15 = 8
+"""
+
+SOLUTIONS.append(solution8)
