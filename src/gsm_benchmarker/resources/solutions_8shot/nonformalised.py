3e8dd4ed0d0eca583f9898ab35b6b9d306b62fb8
mysh <dominika.dlugosz@tecnico.ulisboa.pt>
2026-04-18 09:53:15 +0100

    New ablation study prompt: nonformalised - no "given" or "to find"

---
+++ src/gsm_benchmarker/resources/solutions_8shot/nonformalised.py
@@ -0,0 +1,53 @@
+solution1 = """
+Calculate the difference between trees after planting and trees currently in the grove:
+21 - 15 = 6
+"""
+
+solution2 = """
+Calculate the sum of cars originally in the parking lot and the arriving cars:
+3 + 2 = 5
+"""
+
+solution3 = """
+First, calculate how many chocolates the sisters originally had in total:
+32 + 42 = 74
+Next, subtract the total number of chocolates they ate:
+74 - 35 = 39
+"""
+
+solution4 = """
+Calculate the difference between the lollipops Jason originally had and the lollipops he has now:
+20 - 12 = 8
+"""
+
+solution5 = """
+First, calculate how many toys in total Shawn got for Christmas:
+2 * 2 = 4
+Next, calculate the sum of the number of toys Shawn originally had and the number of new toys he got:
+5 + 4 = 9
+"""
+
+
+solution6 = """
+First, calculate how many new computers were installed in total:
+4 * 5 = 20
+Next, calculate the sum of the number of computers originally in the room and the total number of new computers installed:
+9 + 20 = 29
+"""
+
+solution7 = """
+First, calculate the total number of golf balls Michael lost:
+23 + 2 = 25
+Next, calculate the difference between the original number of golf balls and the number of golf balls lost:
+58 - 25 = 33
+"""
+
+
+solution8 = """
+First, calculate the total cost of the bagels:
+5 * 3 = 15
+Next, calculate the difference between Olivia's original money and what she paid for the bagels:
+23 - 15 = 8
+"""
+
+SOLUTIONS = [solution1, solution2, solution3, solution4, solution5, solution6, solution7, solution8]
