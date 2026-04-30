15b7aaa1138cab67da17105a090c6ddeea5b00dc
mysh <mysh995@gmail.com>
2026-04-30 16:38:39 +0100

    Cleanup

---
+++ src/gsm_benchmarker/results_analyser/utils.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def pandas_to_latex(tab: pd.DataFrame, position: str = 't', **kwargs) -> str:
+    tab = tab.copy()
+
+    # escape LaTeX special characters
+    tab.columns = [str(col).replace('_', '\\_') for col in tab.columns]
+    tab.index = [str(idx).replace('_', '\\_') for idx in tab.index]
+
+    tab.columns.name = None
+
+    # Generate the LaTeX string
+    latex_code = tab.style.format(escape=None).to_latex(
+        column_format='l' + 'c' * len(tab.columns), # No vertical bars
+        position=position,          # ACL prefers 't' (top of page) or 'ht'
+        hrules=True,           # triggers the booktabs lines (\toprule, etc.)
+        **kwargs
+    )
+
+    return latex_code
